0ff59a72c31f41326e2f42808ad585b094cf0e40
matheuscas <matheus.mcas@gmail.com>
2014-05-20 00:07:18 -0300

    added rate to mutation function

--- mutation.m
+++ mutation.m
@@ -1,43 +1,46 @@
-function mutated_individual = mutation( individual )
+function mutated_individual = mutation( individual, rate )
 %mutation Summary of this function goes here
 %   Detailed explanation goes here
 
 individual_size = size(individual);
 individual_size = individual_size(2);
 
-mutation_index = randi([1 individual_size]);
-value_to_mutate = individual(mutation_index);
+for locus = 1:individual_size
+    if randi([0 100]) <= rate
+        %mutation_index = randi([1 individual_size]);
+        value_to_mutate = individual(locus);
 
-if value_to_mutate == 1
-    value_to_mutate = 2;
-elseif value_to_mutate == 2
-        choice = randi([0 1]);
-        if choice == 0
-            value_to_mutate = 1;
-        else
-            value_to_mutate = 3;
-        end         
-elseif value_to_mutate == 3
-        choice = randi([0 1]);
-        if choice == 0
+        if value_to_mutate == 1
             value_to_mutate = 2;
+        elseif value_to_mutate == 2
+                choice = randi([0 1]);
+                if choice == 0
+                    value_to_mutate = 1;
+                else
+                    value_to_mutate = 3;
+                end         
+        elseif value_to_mutate == 3
+                choice = randi([0 1]);
+                if choice == 0
+                    value_to_mutate = 2;
+                else
+                    value_to_mutate = 4;
+                end
+        elseif value_to_mutate == 4
+                choice = randi([0 1]);
+                if choice == 0
+                    value_to_mutate = 3;
+                else
+                    value_to_mutate = 5;
+                end
         else
             value_to_mutate = 4;
         end
-elseif value_to_mutate == 4
-        choice = randi([0 1]);
-        if choice == 0
-            value_to_mutate = 3;
-        else
-            value_to_mutate = 5;
-        end
-else
-    value_to_mutate = 4;
-    
-end
+        individual(locus) = value_to_mutate;
+    end    
+end 
 
 mutated_individual = individual;
-mutated_individual(mutation_index) = value_to_mutate;
 
 end
 
