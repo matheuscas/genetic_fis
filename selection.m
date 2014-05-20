3d7460f20d8a2435bcacd4aea50d9d0c2fde3c8b
matheuscas <matheus.mcas@gmail.com>
2014-05-19 23:36:16 -0300

    added another selection function besides elitism

--- selection.m
+++ selection.m
@@ -1,13 +1,18 @@
-function parents = selection( population, type, quantity )
+function parents = selection( population, type, elite_quantity,quantity_to_select )
 %selection Selects parents from population, based on method
 %   type - selection method type
-%           0 - Elitism, 1 - Rank (not implemented yet), 
+%           0 - Elitism (default), 1 - Rank (not implemented yet), 
 %           2 - Roulette (not implemented yet)
+%           3 - Elitism and Ramdom
 %   quantity - number of individuals that will be selected
 
 %sort population by fitness and gets the best two of them
 population = sort_population_by_fitness(population);
-parents = population(1:quantity);
+parents = population(1:elite_quantity);
+
+if type == 3
+    parents = elite_and_random_selection(population,quantity_to_select,elite_quantity);
+end 
 
 end
 
