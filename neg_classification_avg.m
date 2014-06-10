01acca6804075ca558c18b4f750f899dc63054a8
matheuscas <matheus.mcas@gmail.com>
2014-06-10 20:56:34 -0300

    the average method was count the zeros, bringing the wrong number from average

--- neg_classification_avg.m
+++ neg_classification_avg.m
@@ -1,5 +1,22 @@
 N = dlmread('neg_matrix_file.txt',';');
-N_RES = mean(N,2);
+%N_RES = mean(N,2);
+
+s = size(N);
+lines = s(1);
+columns = s(2);
+N_RES = zeros(lines,1);
+
+for l = 1:lines
+    sum = 0;
+    count = 0;
+    for c = 1:columns
+        if N(l,c) ~= 0
+            sum = sum + N(l,c);
+            count = count + 1;
+        end
+    end
+    N_RES(l) = sum / count;
+end    
 
 N_ERROS = 0; %false positives - FP
 for l = 1:length(N_RES)
--- pos_classification_avg.m
+++ pos_classification_avg.m
@@ -1,5 +1,22 @@
 P = dlmread('pos_matrix_file.txt',';');
-P_RES = mean(P,2);
+% P_RES = mean(P,2);
+
+s = size(P);
+lines = s(1);
+columns = s(2);
+P_RES = zeros(lines,1);
+
+for l = 1:lines
+    sum = 0;
+    count = 0;
+    for c = 1:columns
+        if P(l,c) ~= 0
+            sum = sum + P(l,c);
+            count = count + 1;
+        end
+    end
+    P_RES(l) = sum / count;
+end    
 
 P_ERROS = 0; %false negative - FN
 for l = 1:length(P_RES)
